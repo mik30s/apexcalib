c877c2ec85bd60ec56aee2e474b7c3d620ccad91
Michael Osei <mosei27@live.com>
2019-01-24 14:21:03 -0600

    Update population parameter setup

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
 import numpy as np
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from qrangeslider import QRangeSlider
 
-def setFolderHandler(configvar, window, isfolder=True):
+def setFolderHandler(config,configvar, window, isfolder=True):
     if not isfolder:
-        configvar = QFileDialog.getOpenFileName(
+        config[configvar] = QFileDialog.getOpenFileName(
             window,
             'Observations file', 
             '', 
             'Text files (*.txt)'
         )[0]
     else:
-        configvar = QFileDialog.getExistingDirectory(
+        config[configvar] = QFileDialog.getExistingDirectory(
             window,
             "Open Directory",
             "/Users/michael"
         )
 
-
-def setTextHandler(text, configvar, window):
-    if text.isnumeric(): configvar = int(text)
-    else: configvar = 100
-
+def setValueChangeHandler(value, config, configvar, window):
+    if not type(value) == int:
+        if text.isnumeric(): config[configvar] = int(text)
+        else: config[configvar] = 100
+    else:
+        config[configvar] = value
 
 def computePopulationBias(simulationData, observationData):
     observationSum = sum(np.where(not np.isnan(observationData)))
     simulationSum = sum(np.where(not np.isnan(observationData)) - np.where(not np.isnan(simulationData)))
     return simulationSum / observationSum * 100
-
+ 
 
 def simulate(config):
-    print(config)
+    oldpop = np.array([0]*config['params'])
+    for var in range(config['params']):
+        oldpop[var] = config['minparam'][var] + congfigp['populationSize'] * 
+                      (config['maxparam'][var] - config['minparam'])
+
+    oldpop = np.round(oldpop,5)
+
+    pbiases = np.zeros((config['populationSize'],8))
+
+    simulationOutput = np.zeros((8,3))
+
+    simulationResults = np.zeros(())
+    #print(config)
     for i in range(config['populationSize']):
-        print(i)
-
-
-class AppConfig:
-    def __init__(self):
-        self.inputFolder = None
-        self.outputFolder = None
-        self.observationsFile = None
-        self.populationSize = 0
+        #print(i)
+        
 
 
 def main():
@@ -49,7 +56,10 @@ def main():
         'inputFolder': None,
         'outputFolder': None,
         'observationsFile': None,
-        'populationSize': 0
+        'populationSize': 0,
+        'paramCount': 0,
+        'minparam': np.array([0,0]),
+        'maxparam':np.array([0,0]),
     }
     # init ui
     app  = QApplication([])
@@ -62,13 +72,13 @@ def main():
     # buttons
     # -- set work folder.
     setWorkFolderBtn = QPushButton('Select Work folder')
-    setWorkFolderBtn.clicked.connect(lambda: setFolderHandler(config['inputFolder'], window))
+    setWorkFolderBtn.clicked.connect(lambda: setFolderHandler(config,'inputFolder', window))
     # -- sets output folder.
     setOutputFolderBtn = QPushButton("Select Output folder")
-    setOutputFolderBtn.clicked.connect(lambda: setFolderHandler(config['outputFolder'], window))
+    setOutputFolderBtn.clicked.connect(lambda: setFolderHandler(config, 'outputFolder', window))
     # -- set observations file path
     setObservationsFileBtn = QPushButton("Select Observations")
-    setObservationsFileBtn.clicked.connect(lambda: setFolderHandler(config['observationsFile'], window, isFolder=False))
+    setObservationsFileBtn.clicked.connect(lambda: setFolderHandler(config, 'observationsFile', window, isFolder=False))
     # -- set simulate button
     simulateBtn = QPushButton('Simulate')
     simulateBtn.clicked.connect(lambda: simulate(config))
@@ -76,7 +86,7 @@ def main():
     # textboxes
     # -- set population count textbox
     populationCountTxt = QLineEdit()
-    populationCountTxt.textChanged.connect(lambda text: setTextHandler(text, config['populationSize'], window))
+    populationCountTxt.textChanged.connect(lambda text: setValueChangeHandler(text, config, 'populationSize', window))
 
     # arrange components in ui
     vlayout.addWidget(setWorkFolderBtn)
@@ -88,6 +98,12 @@ def main():
     populationFormSet.addWidget(QLabel('Population Size'))
     populationFormSet.addWidget(populationCountTxt)
     
+    paramFormSet = QHBoxLayout()
+    paramFormSet.addWidget(QLabel('Number of Parameters'))
+    paramValue = QSlider(Qt.Horizontal)
+    paramValue.valueChanged.connect(lambda value: setValueChangeHandler(value, config, 'paramCount', window))
+    paramFormSet.addWidget(paramValue)
+
     maxCalibFormSet = QHBoxLayout()
     maxCalibFormSet.addWidget(QLabel('Max Calibration'))
     maxRange = QRangeSlider()
@@ -103,10 +119,11 @@ def main():
     minCalibFormSet.addWidget(minRange)
 
     vlayout.addLayout(populationFormSet)
+    vlayout.addLayout(paramFormSet)
     vlayout.addLayout(maxCalibFormSet)
     vlayout.addLayout(minCalibFormSet) 
     vlayout.addWidget(simulateBtn)   
-
+    
     hlayout.addLayout(vlayout)
     window.setLayout(hlayout);
     window.show()
